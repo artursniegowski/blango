93d37a658abead7ea7d6d2173b63caa4539b1053
Artur Sniegowski <artur.sniegowski@gmail.com>
2024-02-03 17:59:21 +0000

    Fisnish permissions

--- blog/api/views.py
+++ blog/api/views.py
@@ -1,5 +1,7 @@
 from rest_framework import generics
 from rest_framework.authentication import SessionAuthentication
+from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
+from rest_framework.permissions import IsAdminUser
 
 from blog.api.serializers import PostSerializer
 from blog.models import Post
@@ -13,5 +15,8 @@ class PostList(generics.ListCreateAPIView):
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    # However IsAdminUser always returns True from has_object_permission(), even if a user isn’t admin! So by using it we’ll give all permissions to everyone.
+    # permission_classes = [AuthorModifyOrReadOnly | IsAdminUser]
+    permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
