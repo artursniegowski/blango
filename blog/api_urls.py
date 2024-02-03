36bcbae8a544637b3253020b168a848871df3238
Artur Sniegowski <artur.sniegowski@gmail.com>
2024-02-03 11:32:56 +0000

    Finished DRF views

--- blog/api_urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from blog import api_views
-
-urlpatterns = [
-    path("posts/", api_views.post_list, name="api_post_list"),
-    path("posts/<int:pk>/", api_views.post_detail, name="api_post_detail"),
-]
