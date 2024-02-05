730e192117833a8faf8778f372d4e36c0cd95dae
Artur Sniegowski <artur.sniegowski@gmail.com>
2024-02-05 10:43:23 +0000

    Finish throtteling

---
+++ blog/api/throttling.py
@@ -0,0 +1,18 @@
+from rest_framework.throttling import AnonRateThrottle, UserRateThrottle
+
+
+# custom throttle classes
+class AnonSustainedThrottle(AnonRateThrottle):
+    scope = "anon_sustained"
+
+
+class AnonBurstThrottle(AnonRateThrottle):
+    scope = "anon_burst"
+
+
+class UserSustainedThrottle(UserRateThrottle):
+    scope = "user_sustained"
+
+
+class UserBurstThrottle(UserRateThrottle):
+    scope = "user_burst"
