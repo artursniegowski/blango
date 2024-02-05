2cbd556c827f8e56f49acb3e7c25dfea9fa263d5
Artur Sniegowski <artur.sniegowski@gmail.com>
2024-02-05 16:04:40 +0000

    Finish simplejwt

--- blango/settings.py
+++ blango/settings.py
@@ -14,7 +14,7 @@ import os
 from pathlib import Path
 from configurations import Configuration, values
 import dj_database_url
-
+from datetime import timedelta
 
 class Dev(Configuration):
     # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -245,6 +245,8 @@ class Dev(Configuration):
             "rest_framework.authentication.BasicAuthentication",
             "rest_framework.authentication.SessionAuthentication",
             "rest_framework.authentication.TokenAuthentication",
+            # adding JWT
+            "rest_framework_simplejwt.authentication.JWTAuthentication",
         ],
         "DEFAULT_PERMISSION_CLASSES": [
             "rest_framework.permissions.IsAuthenticatedOrReadOnly"
@@ -282,6 +284,12 @@ class Dev(Configuration):
         }
     }
 
+    # settings for JWT - exteding the token life time - not recomenede, only for
+    # developent to make it easier
+    SIMPLE_JWT = {
+        "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+        "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    }
 
 class Prod(Dev):
     DEBUG = values.BooleanValue(True)
--- blog/api/urls.py
+++ blog/api/urls.py
@@ -6,6 +6,7 @@ from drf_yasg.views import get_schema_view
 import os
 from blog.api.views import PostViewSet, UserDetail, TagViewSet
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 schema_view = get_schema_view(
@@ -43,6 +44,9 @@ urlpatterns += [
         schema_view.without_ui(cache_timeout=0),
         name="schema-json",
     ),
+    # for JWT
+    path("jwt/", TokenObtainPairView.as_view(), name="jwt_obtain_pair"),
+    path("jwt/refresh/", TokenRefreshView.as_view(), name="jwt_refresh"),
     path(
         "swagger/",
         schema_view.with_ui("swagger", cache_timeout=0),
